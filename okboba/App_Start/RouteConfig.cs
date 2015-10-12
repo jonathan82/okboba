6fea562c34a669d2856f6ae130fe773e013ec8d2
Jonathan <jonlin82@gmail.com>
2015-10-11 23:26:01 -0700

    ok

--- okboba/App_Start/RouteConfig.cs
+++ okboba/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace okboba
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Index",
+                url: "{controller}/{action}/{id}",
+                defaults: new { action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Account", action = "Register", id = UrlParameter.Optional }
