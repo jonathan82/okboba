671bac0fe6d53f6e1a87781f34156ff3f034696e
Jonathan <jonlin82@gmail.com>
2015-11-01 22:23:27 -0800

    ok

--- okboba.MatchApi/Controllers/MatchesController.cs
+++ okboba.MatchApi/Controllers/MatchesController.cs
@@ -7,8 +7,20 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+/// <summary>
+/// MatchApi contains API controllers to get matches for a user.  It will return only what's necessary
+/// (ie for a given page).  It will first look in the cache, and if not there call MatchCalc to get the 
+/// matches, put them in the cache, and return them to the caller.
+/// 
+/// Possible Flow 1: user ===> okboba.Web ===> okboba.MatchApi
+/// 
+/// Possible Flow 2: user ===> okboba.MatchApi (user calls MatchApi directly using jquery)
+/// 
+/// </summary>
+
+
 namespace okboba.MatchApi.Controllers
-{
+{        
     public class MatchesController : ApiController
     {
         private IMatchRepository _matchRepo;
